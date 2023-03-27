67a577bb9bd31b30fd42c20d5ef9fd505506c391
AlmogShor <75311629+AlmogShor@users.noreply.github.com>
2023-03-27 10:24:36 +0300

    starting tests

--- Test.cpp
+++ Test.cpp
@@ -4,6 +4,9 @@
 
 #include "doctest.h"
 #include <iostream>
+
+#ןמבךוגק <stdexcept>
+
 #include <cassert>
 #include "sources/player.hpp"
 #include "sources/card.hpp"
@@ -14,11 +17,27 @@ using namespace ariel;
 
 TEST_CASE("Test 1") {
 
-    SUBCASE("stack size") {
+    Player p_1("Aba")
+    Player p_2("Ima")
 
+    //create a game
+    Game game(p_1, p_2);
 
-
+    SUBCASE("init - stacksize") {
+        CHECK(p_1.stacksize() == 26);
+        CHECK(p_2.stacksize() == 26);
+        CHECK(p_1.cardesTaken() == 0);
+        CHECK(p_2.cardesTaken() == 0);
     }
+//    SUBCASE("init - win rate") {
+//        CHECK(p_1.getWinRate() == 0);
+//        CHECK(p_2.getWinRate() == 0);
+//    }
+
+    SUBCASE("init- get winner"){
+        CHECK(game.printWiner())
+    }
+
 }
 
 
